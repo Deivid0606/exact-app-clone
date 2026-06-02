9352315f7f784f0f7ddaee502721a59bedc96805
Deivid0606 <deividaguilar06@gmail.com>
2026-06-02 15:00:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom"; // 👈 HashRouter
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -17,15 +17,14 @@ const App = () => (
       <AuthProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter> {/* 👈 HashRouter */}
           <Routes>
             <Route path="/" element={<Index />} />
-            <Route path="/asignar-pedidos/:id" element={<AssignOrdersView />} />
             <Route path="/asignar-pedidos" element={<AssignOrdersView />} />
             <Route path="/qr" element={<QRScannerView />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </TooltipProvider>
   </QueryClientProvider>
