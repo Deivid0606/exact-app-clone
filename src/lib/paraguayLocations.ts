950b4523a02495acbf8c926d2ba3c205b46ec7ce
Deivid0606 <deividaguilar06@gmail.com>
2026-08-15 21:07:59 -0300

    Add Paraguay locations and utility functions
    This file defines types and constants for Paraguay's departments and their cities, including a function to retrieve cities by department and a function to create a location key.

---
+++ src/lib/paraguayLocations.ts
@@ -0,0 +1,358 @@
+export type ParaguayDepartment = { department: string; cities: string[] };
+
+export const PARAGUAY_LOCATIONS: ParaguayDepartment[] = [
+  {
+    "department": "Asunción",
+    "cities": [
+      "Asunción"
+    ]
+  },
+  {
+    "department": "Concepción",
+    "cities": [
+      "Concepción",
+      "Belén",
+      "Horqueta",
+      "Loreto",
+      "San Carlos del Apa",
+      "San Lázaro",
+      "Yby Yaú",
+      "Azotey",
+      "Sargento José Félix López",
+      "San Alfredo",
+      "Paso Barreto",
+      "Arroyito"
+    ]
+  },
+  {
+    "department": "San Pedro",
+    "cities": [
+      "San Pedro de Ycuamandyyú",
+      "Antequera",
+      "Choré",
+      "General Elizardo Aquino",
+      "Itacurubí del Rosario",
+      "Lima",
+      "Nueva Germania",
+      "San Estanislao",
+      "San Pablo",
+      "Tacuatí",
+      "Unión",
+      "25 de Diciembre",
+      "Villa del Rosario",
+      "General Isidoro Resquín",
+      "Yataity del Norte",
+      "Guayaibí",
+      "Capiibary",
+      "Santa Rosa del Aguaray",
+      "Yrybucuá",
+      "Liberación",
+      "San Vicente Pancholo"
+    ]
+  },
+  {
+    "department": "Cordillera",
+    "cities": [
+      "Caacupé",
+      "Altos",
+      "Arroyos y Esteros",
+      "Atyrá",
+      "Caraguatay",
+      "Emboscada",
+      "Eusebio Ayala",
+      "Isla Pucú",
+      "Itacurubí de la Cordillera",
+      "Juan de Mena",
+      "Loma Grande",
+      "Mbocayaty del Yhaguy",
+      "Nueva Colombia",
+      "Piribebuy",
+      "Primero de Marzo",
+      "San Bernardino",
+      "Santa Elena",
+      "Tobatí",
+      "Valenzuela"
+    ]
+  },
+  {
+    "department": "Guairá",
+    "cities": [
+      "Villarrica",
+      "Borja",
+      "Capitán Mauricio José Troche",
+      "Coronel Martínez",
+      "Félix Pérez Cardozo",
+      "General Eugenio A. Garay",
+      "Independencia",
+      "Itapé",
+      "Iturbe",
+      "José Fassardi",
+      "Mbocayaty",
+      "Natalicio Talavera",
+      "Ñumí",
+      "San Salvador",
+      "Yataity",
+      "Doctor Bottrell",
+      "Paso Yobái",
+      "Tebicuary"
+    ]
+  },
+  {
+    "department": "Caaguazú",
+    "cities": [
+      "Coronel Oviedo",
+      "Caaguazú",
+      "Carayaó",
+      "Doctor Cecilio Báez",
+      "Santa Rosa del Mbutuy",
+      "Doctor Juan Manuel Frutos",
+      "Repatriación",
+      "Nueva Londres",
+      "San Joaquín",
+      "San José de los Arroyos",
+      "Yhú",
+      "Doctor J. Eulogio Estigarribia",
+      "R.I. 3 Corrales",
+      "Raúl Arsenio Oviedo",
+      "José Domingo Ocampos",
+      "Mariscal Francisco Solano López",
+      "La Pastora",
+      "Tres de Febrero",
+      "Simón Bolívar",
+      "Vaquería",
+      "Tembiaporá",
+      "Nueva Toledo"
+    ]
+  },
+  {
+    "department": "Caazapá",
+    "cities": [
+      "Caazapá",
+      "Abaí",
+      "Buena Vista",
+      "Doctor Moisés S. Bertoni",
+      "General Higinio Morínigo",
+      "Maciel",
+      "San Juan Nepomuceno",
+      "Tavaí",
+      "Yegros",
+      "Yuty",
+      "3 de Mayo"
+    ]
+  },
+  {
+    "department": "Itapúa",
+    "cities": [
+      "Encarnación",
+      "Bella Vista",
+      "Cambyretá",
+      "Capitán Meza",
+      "Capitán Miranda",
+      "Nueva Alborada",
+      "Carmen del Paraná",
+      "Coronel Bogado",
+      "Carlos Antonio López",
+      "Natalio",
+      "Fram",
+      "General Artigas",
+      "General Delgado",
+      "Hohenau",
+      "Jesús",
+      "José Leandro Oviedo",
+      "Obligado",
+      "Mayor Julio Dionisio Otaño",
+      "San Cosme y Damián",
+      "San Pedro del Paraná",
+      "San Rafael del Paraná",
+      "Trinidad",
+      "Edelira",
+      "Tomás Romero Pereira",
+      "Alto Verá",
+      "La Paz",
+      "Yatytay",
+      "San Juan del Paraná",
+      "Pirapó",
+      "Itapúa Poty"
+    ]
+  },
+  {
+    "department": "Misiones",
+    "cities": [
+      "San Juan Bautista",
+      "Ayolas",
+      "San Ignacio",
+      "San Miguel",
+      "San Patricio",
+      "Santa María",
+      "Santa Rosa",
+      "Santiago",
+      "Villa Florida",
+      "Yabebyry"
+    ]
+  },
+  {
+    "department": "Paraguarí",
+    "cities": [
+      "Paraguarí",
+      "Acahay",
+      "Caapucú",
+      "Caballero",
+      "Carapeguá",
+      "Escobar",
+      "La Colmena",
+      "Mbuyapey",
+      "Pirayú",
+      "Quiindy",
+      "Quyquyhó",
+      "San Roque González de Santa Cruz",
+      "Sapucái",
+      "Tebicuarymí",
+      "Yaguarón",
+      "Ybycuí",
+      "Ybytymí",
+      "María Antonia"
+    ]
+  },
+  {
+    "department": "Alto Paraná",
+    "cities": [
+      "Ciudad del Este",
+      "Presidente Franco",
+      "Domingo Martínez de Irala",
+      "Doctor Juan León Mallorquín",
+      "Hernandarias",
+      "Itakyry",
+      "Juan E. O'Leary",
+      "Ñacunday",
+      "Yguazú",
+      "Los Cedrales",
+      "Minga Guazú",
+      "San Cristóbal",
+      "Santa Rita",
+      "Naranjal",
+      "Santa Rosa del Monday",
+      "Minga Porã",
+      "Mbaracayú",
+      "San Alberto",
+      "Iruña",
+      "Santa Fe del Paraná",
+      "Tavapy",
+      "Doctor Raúl Peña"
+    ]
+  },
+  {
+    "department": "Central",
+    "cities": [
+      "Areguá",
+      "Capiatá",
+      "Fernando de la Mora",
+      "Guarambaré",
+      "Itá",
+      "Itauguá",
+      "Lambaré",
+      "Limpio",
+      "Luque",
+      "Mariano Roque Alonso",
+      "Nueva Italia",
+      "Ñemby",
+      "San Antonio",
+      "San Lorenzo",
+      "Villa Elisa",
+      "Villeta",
+      "Ypacaraí",
+      "Ypané",
+      "J. Augusto Saldívar"
+    ]
+  },
+  {
+    "department": "Ñeembucú",
+    "cities": [
+      "Pilar",
+      "Alberdi",
+      "Cerrito",
+      "Desmochados",
+      "General José Eduvigis Díaz",
+      "Guazú Cuá",
+      "Humaitá",
+      "Isla Umbú",
+      "Laureles",
+      "Mayor José J. Martínez",
+      "Paso de Patria",
+      "San Juan Bautista de Ñeembucú",
+      "Tacuaras",
+      "Villa Franca",
+      "Villa Oliva",
+      "Villalbín"
+    ]
+  },
+  {
+    "department": "Amambay",
+    "cities": [
+      "Pedro Juan Caballero",
+      "Bella Vista Norte",
+      "Capitán Bado",
+      "Zanja Pytã",
+      "Karapaí",
+      "Cerro Corá"
+    ]
+  },
+  {
+    "department": "Canindeyú",
+    "cities": [
+      "Salto del Guairá",
+      "Corpus Christi",
+      "Villa Ygatimí",
+      "Itanará",
+      "Ypejhú",
+      "Francisco Caballero Álvarez",
+      "Katueté",
+      "La Paloma del Espíritu Santo",
+      "Nueva Esperanza",
+      "Yasy Cañy",
+      "Ybyrarobaná",
+      "Yby Pytã",
+      "Maracaná",
+      "Puerto Adela",
+      "Laurel"
+    ]
+  },
+  {
+    "department": "Presidente Hayes",
+    "cities": [
+      "Villa Hayes",
+      "Benjamín Aceval",
+      "Puerto Pinasco",
+      "Nanawa",
+      "José Falcón",
+      "Teniente Primero Manuel Irala Fernández",
+      "Teniente Esteban Martínez",
+      "General José María Bruguez",
+      "Campo Aceval",
+      "Nueva Asunción"
+    ]
+  },
+  {
+    "department": "Boquerón",
+    "cities": [
+      "Mariscal Estigarribia",
+      "Filadelfia",
+      "Loma Plata",
+      "Boquerón"
+    ]
+  },
+  {
+    "department": "Alto Paraguay",
+    "cities": [
+      "Fuerte Olimpo",
+      "Bahía Negra",
+      "Puerto Casado",
+      "Carmelo Peralta"
+    ]
+  }
+] as ParaguayDepartment[];
+
+export const citiesForDepartment = (department: string) =>
+  PARAGUAY_LOCATIONS.find((item) => item.department === department)?.cities || [];
+
+export const locationKey = (department: string, city: string) =>
+  `${department}|||${city}`;
